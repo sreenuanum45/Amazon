a4755e86d65afcf202967966862c0cabc7cd3324
Sreenuanum45 <anumandlasreenu@gmail.com>
2025-11-13 23:39:10 +0530

    updated scripts

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -31,7 +31,7 @@ import org.testng.annotations.DataProvider;
 public class TestRunner extends AbstractTestNGCucumberTests {
 
     @Override
-    @DataProvider(parallel = false)
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
